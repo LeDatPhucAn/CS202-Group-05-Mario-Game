b0b7a3f8b75e795172e904f674d46f620979c2cd
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-07-13 21:54:46 +0700

    deleted codes in BlockFactory (will implement later)

--- header/MyMap/BlockFactory.hpp
+++ header/MyMap/BlockFactory.hpp
@@ -1,12 +1,1 @@
-/*
-#pragma once
-using namespace std;
-#include "tileson.hpp"
-#include "raylib.h"
-#include "Block.hpp"
-
-class BlockFactory {
-public:
-	static Block* create(tson::Object& obj, Vector2 pos, Vector2 size, Texture2D tex, Rectangle src);
-};
-*/
+// TO-DO: implement
